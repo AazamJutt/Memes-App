58ccb19c5b5abb4bf44907cb6b24b5f7d325e7ab
Aazam Jutt <m.aazasultan@gmail.com>
2021-12-10 01:44:16 +0500

    Tried fetching data, gave error

--- app/src/main/java/com/example/memeshareapp/MainActivity.java
+++ app/src/main/java/com/example/memeshareapp/MainActivity.java
@@ -4,13 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.app.Application;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recylerView;
     LinearLayoutManager layoutManager;
+    List<Meme> memes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +40,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recylerView = findViewById(R.id.recyclerView);
         layoutManager = new LinearLayoutManager(this);
-        ArrayList<Meme> memes = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Meme meme = new Meme("Sheed","Kese ho yar?","www.google.com","");
-            memes.add(meme);
-        }
-        MemeAdapter memeAdapter = new MemeAdapter(memes);
+        memes = new ArrayList<>();
+        MemeAdapter memeAdapter = new MemeAdapter(this, memes);
+        Log.d("TAG", "onCreate: memes = "+memes.size());
         recylerView.setAdapter(memeAdapter);
         recylerView.setLayoutManager(layoutManager);
+        fetchMemes();
+        memeAdapter.notifyDataSetChanged();
+    }
 
+    public void fetchMemes() {
+        String url = "https://meme-api.herokuapp.com/gimme/wholesomememes/" + "4";
+
+        // Initialize a new RequestQueue instance
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        // Initialize a new JsonArrayRequest instance
+        JsonObjectRequest jsonArrayRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                        url,
+                        null,
+                        response -> {
+
+                            try {
+                                JSONArray jsonArray = response.getJSONArray("memes");
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject obj = jsonArray.getJSONObject(i);
+                                    String author = obj.getString("author");
+                                    String caption = obj.getString("title");
+                                    String postLink = obj.getString("postLink");
+                                    String imageUrl = obj.getString("url");
+                                    Log.d("url", "fetchMemes: " + imageUrl);
+                                    if(!imageUrl.endsWith("gif")) {
+                                        Meme meme = new Meme(author, caption, imageUrl, postLink);
+                                        memes.add(meme);
+                                    }
+                                }
+                                Log.d("Response", "fetchMemes: "+response);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        },
+                        error -> Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show());
+        requestQueue.add(jsonArrayRequest);
     }
 }
--- app/src/main/java/com/example/memeshareapp/MemeAdapter.java
+++ app/src/main/java/com/example/memeshareapp/MemeAdapter.java
@@ -1,5 +1,7 @@
 package com.example.memeshareapp;
 
+import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,13 +11,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class MemeAdapter extends RecyclerView.Adapter<MemeAdapter.MemeViewHolder> {
     List<Meme> memes;
+    Activity context;
 
-    public MemeAdapter(List<Meme> memes) {
+    public MemeAdapter(Activity context, List<Meme> memes) {
         this.memes = memes;
+        this.context = context;
     }
 
 
@@ -23,15 +29,17 @@ public class MemeAdapter extends RecyclerView.Adapter<MemeAdapter.MemeViewHolder
     @Override
     public MemeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.meme_post, parent, false);
-        return new MemeViewHolder(view);
+        MemeViewHolder holder = new MemeViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MemeViewHolder holder, int position) {
-        Meme meme = memes.get(position);
-        holder.caption.setText(meme.getCaption());
-        holder.author.setText(meme.getAuthor());
-
+        Meme currMeme = memes.get(position);
+        holder.caption.setText(currMeme.getCaption());
+        holder.author.setText(currMeme.getAuthor());
+        if (position == memes.size() - 1)
+            Glide.with(context).load(currMeme.url).into(holder.meme);
     }
 
     @Override
